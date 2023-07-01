052a29c7dd0dc1b96565eb98e01d95a6f7cfe3ea
Koichi5 <koichi20021217@gmail.com>
2023-07-01 18:10:11 +0900

    add swipe cards

--- lib/view/itnitial_page.dart
+++ lib/view/itnitial_page.dart
@@ -1,13 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:swipe_cards/draggable_card.dart';
 import 'package:swipe_cards/swipe_cards.dart';
 
-class InitialPage extends StatelessWidget {
-  InitialPage({Key? key}) : super(key: key);
+class InitialPage extends StatefulWidget {
+  const InitialPage({Key? key}) : super(key: key);
 
-  final List<SwipeItem> _swipeItems = [];
+  @override
+  State<InitialPage> createState() => _InitialPageState();
+}
+
+class _InitialPageState extends State<InitialPage> {
+  List<SwipeItem> _swipeItems = [];
   late MatchEngine _matchEngine;
   GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey();
   List<String> _names = ["Red", "Blue", "Green", "Yellow", "Orange"];
+  List<Map <String, int>> questionList = [];
   List<Color> _colors = [
     Colors.red,
     Colors.blue,
@@ -15,10 +22,128 @@ class InitialPage extends StatelessWidget {
     Colors.yellow,
     Colors.orange
   ];
+
+  @override
+  void initState() {
+    for (int i = 0; i < _names.length; i++) {
+      _swipeItems.add(SwipeItem(
+          content: Content(text: _names[i], color: _colors[i]),
+          likeAction: () {
+            ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+              content: Text("Liked ${_names[i]}"),
+              duration: Duration(milliseconds: 500),
+            ));
+          },
+          nopeAction: () {
+            ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+              content: Text("Nope ${_names[i]}"),
+              duration: Duration(milliseconds: 500),
+            ));
+          },
+          superlikeAction: () {
+            ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+              content: Text("Superliked ${_names[i]}"),
+              duration: Duration(milliseconds: 500),
+            ));
+          },
+          onSlideUpdate: (SlideRegion? region) async {
+            print("Region $region");
+          }));
+    }
+
+    _matchEngine = MatchEngine(swipeItems: _swipeItems);
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text("initial page")),
-    );
+        key: _scaffoldKey,
+        appBar: AppBar(
+          title: Text("initial page"),
+        ),
+        body: Container(
+            child: Column(children: [
+              Container(
+                height: 550,
+                child: SwipeCards(
+                  matchEngine: _matchEngine,
+                  itemBuilder: (BuildContext context, int index) {
+                    return Container(
+                      alignment: Alignment.center,
+                      color: _swipeItems[index].content.color,
+                      child: Text(
+                        _swipeItems[index].content.text,
+                        style: TextStyle(fontSize: 100),
+                      ),
+                    );
+                  },
+                  onStackFinished: () {
+                    ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                      content: Text("Stack Finished"),
+                      duration: Duration(milliseconds: 500),
+                    ));
+                  },
+                  itemChanged: (SwipeItem item, int index) {
+                    print("item: ${item.content.text}, index: $index");
+                  },
+                  upSwipeAllowed: true,
+                  fillSpace: true,
+                ),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  ElevatedButton(
+                      onPressed: () {
+                        // _matchEngine.currentItem.decision
+                        print("Nope");
+                      },
+                      child: Text("Nope")),
+                  // ElevatedButton(
+                  //     onPressed: () {
+                  //       // _matchEngine.currentItem.superLike();
+                  //       print("SuperLike");
+                  //     },
+                  //     child: Text("Superlike")),
+                  ElevatedButton(
+                      onPressed: () {
+                        // _matchEngine.currentItem.like();
+                        print("Like");
+                      },
+                      child: Text("Like"))
+                ],
+              )
+            ])));
   }
 }
+
+class Content {
+  final String text;
+  final Color color;
+
+  Content({required this.text, required this.color});
+}
+
+
+// class InitialPage extends StatefulWidget {
+//   InitialPage({Key? key}) : super(key: key);
+//
+//   final List<SwipeItem> _swipeItems = [];
+//   late MatchEngine _matchEngine;
+//   GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey();
+//   List<String> _names = ["Red", "Blue", "Green", "Yellow", "Orange"];
+//   List<Color> _colors = [
+//     Colors.red,
+//     Colors.blue,
+//     Colors.green,
+//     Colors.yellow,
+//     Colors.orange
+//   ];
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(title: const Text("initial page")),
+//     );
+//   }
+// }
